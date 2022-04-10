5e9614a1ce8b24d1cd885ed83f5ca465dff79c2d
Christophe Laborde <christophelaborde@protonmail.com>
2022-04-10 21:26:02 +0200

    premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-todo-list';
+  items = " ";
+
+
+onSubmit(dataForm: any) {
+    console.log(dataForm.form.value.todoItem);
+    
+  }
+  
 }
